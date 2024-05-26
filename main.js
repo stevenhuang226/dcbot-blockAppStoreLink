4d0c2f9649720dff9977746e6e90cc71e730a1e2
stevenhuang226 <stevenhuang.dog@gmail.com>
2024-05-26 09:57:11 +0800

    ready

--- main.js
+++ main.js
@@ -1,55 +1,30 @@
-const { Client, GatewayIntentBits } = require("discord.js");
 const rl = require("readline");
 const readline = rl.createInterface({
 	input: process.stdin,
 	output: process.stdout, 
 });
 const logger = require("./shdlib/logger.js");
-var TOKEN = "";
-var LOGPATH = "";
-async function setTOK() {
-	await new Promise( async (resolve) => {
-		readline.question("input token:", (ans) =>{
-			TOKEN = ans;
-			resolve();
+const { Client, GatewayIntentBits } = require("discord.js");
+const { TOKEN, LOGPATH } = require("./config.json")
+console.log("token= %s", TOKEN);
+console.log("log path= %s", LOGPATH);
+const client = new Client({intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
+]});
+client.on("ready", () => {
+	console.log(`logged in as ${client.user.tag}`)
+	logger.write(LOGPATH, `log in as: ${client.user.tag}`)
+});
+client.on("messageCreate", async (message) => {
+	if ( /apps.apple.com/.test(message.content) ) {
+		await message.delete().then( message => {
+			logger.write(LOGPATH, `delete:${message.content} from ${message.author.username}`)
 		});
-	}).then( () => {
-		return 0;
-	} )
-}
-async function setLogPath() {
-	const fs = require("node:fs");
-	await new Promise( async (resolve) => {
-		readline.question("input log path:", async (ans) => {
-			await fs.access(ans, fs.constants.W_Ok, async (error) => {
-				if ( error ) {
-					await setLogPath();
-					resolve();
-				}
-				else {
-					LOGPATH = ans;
-					resolve();
-				}
-			});
-		});
-	});
-}
-async function main() {
-	const { Client, GatewayIntentBits } = require("discord.js");
-	await setTOK();
-	await setLogPath();
-	console.log("token= %s", TOKEN);
-	console.log("log path= %s", LOGPATH);
-	client.on("ready", () => {
-		console.log(`logged in as ${client.user.tag}`)
-	});
-	client.on("messageCreate", async (message) => {
-		if ( /apps.apple.com/.test(message.content) ) {
-			logger.write(LOGPATH, "block" + message.content);
-		}
-	});
-	client.on("error", (error) => {
-		logger.write(LOGPATH, "error" + error);
-	});
-}
-main();
+	}
+});
+client.on("error", (error) => {
+	logger.write(LOGPATH, "error:" + error);
+});
+client.login(TOKEN);
